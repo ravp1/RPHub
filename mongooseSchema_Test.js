9d3b531e673d0a2e2328ddf2d2907423e58c3b0d
ravp1 <ravp1@ymail.com>
2014-06-13 14:54:20 -0400

    Changes made

--- mongooseSchema_Test.js
+++ mongooseSchema_Test.js
@@ -5,32 +5,8 @@ var express = require('express'),
   mailer=require("nodemailer"),
   crypto=require("crypto"),
   bodyParser = require("body-parser");
-
-//Setup the server to listen on port 80 (Web traffic port), allow it to parse POSTED body data, and let it render EJS pages 
-server.listen(80);
-app.use(bodyParser());
-//app.set('view engine', 'ejs');
-
-
-//http.createServer(function (req, res) {
-//  res.writeHead(200, {'Content-Type': 'text/plain'});
-//  res.end('Hello World\n');
-//}).listen(8080, '127.0.0.1');
-app.get('/', function(req, res){
-	  res.send('Hello World');
-});
-
-/*var server = app.listen(80, function() {
-    console.log('Listening on port %d', server.address().port);
-	
-	
-});*/
-console.log('Server running at http://127.0.0.1:8080/');
-
-
-
-
 var mongoose = require('mongoose');
+
 mongoose.connect('mongodb://localhost/test');
 
 var db = mongoose.connection;
@@ -55,17 +31,76 @@ var categorySchema = new Schema({
 
 var Category = mongoose.model('Category', categorySchema);
 
+Category.remove({},function(err){
+	console.log("removed categores");
+});
+
 var engineering = new Category({ name: 'engineering'});
 engineering.interests.push({name:'electrical engineering'});
 engineering.interests[0].posts.push({poster:'Dr. ABC', content:'For students looking for experience with digital design', title:'VLSI Project'});
 engineering.save();
-console.log(engineering);
+//console.log(engineering);
 
 var performance = new Category({name:'performance'});
 performance.interests.push({name:'pop music'});
 performance.interests[0].posts.push({poster:'Dr. ABC', content:"Performance during my daughter's graduation party. Formal attire requested.", title:'Performance June 21'});
 performance.save();
-console.log(performance);
+//console.log(performance);
+
+
+
+
+//Setup the server to listen on port 80 (Web traffic port), allow it to parse POSTED body data, and let it render EJS pages 
+server.listen(80);
+app.use(bodyParser());
+//app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname));
+
+//http.createServer(function (req, res) {
+//  res.writeHead(200, {'Content-Type': 'text/plain'});
+//  res.end('Hello World\n');
+//}).listen(8080, '127.0.0.1');
+app.get('/', function(req, res){
+	  res.redirect('index.html');
+});
+
+/*var server = app.listen(80, function() {
+    console.log('Listening on port %d', server.address().port);
+	
+	
+});*/
+console.log('Server running at http://127.0.0.1:8080/');
+console.log("This is an arbitrary change");
+
+
+
+app.get("/categories",function(req,res){
+
+	Category.find({},function(err,data){
+	
+		console.log("found categories. data.length is " + data.length);
+	
+		/*var response = {names:[]};
+		for (var i = 0; i < data.length; i++)
+		{
+			response.names.push(data[i].name);
+		}
+		console.log(response);
+		res.write(JSON.stringify(response) );*/
+		res.write(JSON.stringify(data) );
+		res.end();
+	});
+
+});
+
+
+
+
+
+
+
+/*
 
 function makeNewCategory(name, interests)
 {
@@ -74,7 +109,7 @@ function makeNewCategory(name, interests)
 
 categorySchema.methods.findCommonPosters = function(cb) {
 	return this.model('Category').find({interests: [{posts:[{poster: this.interests.posts.poster}] }] }, cb);
-}
+}*/
 /* example function call:
 	engineering.findCommonPosters(function(err, categories){
 		console.log(categories);

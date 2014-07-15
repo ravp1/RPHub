ca5d3f5587e83f740b312777d7fd1368ebcd595d
everything1burrito <henry.p.choi@gmail.com>
2014-07-15 00:49:05 -0400

    new revamped interest

---
+++ runserver.bat
@@ -0,0 +1,3 @@
+
+start cmd /c "cd C:\Program Files\MongoDB 2.6 Standard\bin\ && C: && mongod.exe"
+start cmd /k "cd C:\Program Files\nodejs\ && C: && nodevars.bat && cd D:\Documents\Github\RPHub\ && D: && node test.js"
